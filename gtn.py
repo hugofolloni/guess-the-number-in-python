cb2c1a26fb8e0413049d2fdbda80e772a711fe7e
Hugo Folloni <hugofollogua07@gmail.com>
2021-07-06 15:09:45 -0300

    Added a loop

--- gtn.py
+++ gtn.py
@@ -1,38 +1,53 @@
-from random import seed
-from random import randint
+import random
+import os
 
-seed(1)
+
 
 guessarray = []
+tries = []
 
 print("Guess the random number between 0 and 100! Good luck!") # Initial term
 
+def gameagain(): 
+    playagain = input("Do you want to play again? Please, answer with 'y' or 'n'! ")
+    if playagain == 'n':
+        exit()
+    if playagain == "y":
+        os.system("python gtn.py")
+        print ("Restarting...")
+        exit()
+ 
 def checknumber(): # function that work the guesses
-    guess = int(input("Your try: ")) # input
+    guessinput = input("Your try: ") # input
+    if guessinput == '': # if the input.value is undefined, reads '0'
+        guessinput = 0
+    guess = int(guessinput)
     if guess == value:
-        print("Right")
+        print("You're right! Congratulations!")
+        return gameagain()
     if guess > value:
         print("Wrong! Too high")
         guessarray.append(guess) # add the current guess at the guesses list
         print("You guesses was: {}".format(guessarray)) # display the guess list
+        tries.append('.') # append on the try list a new dot, to count how many tries i did
         return loopstoper() # call the function that work with the loop
     if guess < value:
         print("Wrong! Too low!")
         guessarray.append(guess)
         print("You guesses was: {}".format(guessarray))
+        tries.append('.')
         return loopstoper()
 
 
 def loopstoper():
-    tries = 0
-    if tries < 10:
+    if len(tries) < 10: # check the length of 'tries'. If < 10, you can try again. If not, just the lost label.
         return checknumber()
-    else: 
+    if len(tries) == 10: 
         print("You've lost!")
-    print(tries)
+        return gameagain()
 
-value = randint(0, 100)
-print(value)
+value = random.randint(0, 100)
 loopstoper()
+
 
 ## loop isn't working
